12c55ba52c6e49e0331fc5fb4eb2e1ae39f09fa7
Jhonata Costa <jhonata_costa@econeteditora.com.br>
2022-07-21 08:28:48 -0300

    adicionando fotos e estilizacoes da sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,20 @@ import styles from './Sidebar.module.css';
 
 export function Sidebar() {
   return (
-    <aside className={styles.sidebar}> Sidebar </aside>
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover} 
+        src="https://images.unsplash.com/photo-1475924156734-496f6cac6ec1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+      />
+
+      <div className={styles.profile}>
+        <strong>Jhonata Costa</strong>
+        <span>Developer</span>
+
+        <footer>
+          <a href="#">Editar perfil</a>
+        </footer>
+      </div>
+    </aside>
   )
 }
